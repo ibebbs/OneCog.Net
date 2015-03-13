25aabea92f86473aacefeb44ce22df5a04360f64
ibebbs <github@bebbs.co.uk>
2015-03-13 08:20:06 +0000

    Initial commit

---
+++ src/OneCog.Net.Desktop/TcpClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using CoreTcpClient = System.Net.Sockets.TcpClient;
+
+namespace OneCog.Net
+{
+    public class TcpClient : ITcpClient
+    {
+        private CoreTcpClient _tcpClient;
+        private NetworkStream _networkStream;
+
+        public async Task Connect(string host, uint port)
+        {
+            _tcpClient = new CoreTcpClient();
+            await _tcpClient.ConnectAsync(host, (int)port);
+            _networkStream = _tcpClient.GetStream();
+        }
+
+        public Task Read(byte[] bytes)
+        {
+            if (bytes == null) throw new ArgumentNullException("bytes");
+
+            return _networkStream.ReadAsync(bytes, 0, bytes.Length);
+        }
+
+        public Task Write(byte[] bytes)
+        {
+            if (bytes == null) throw new ArgumentNullException("bytes");
+
+            return _networkStream.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        public void Dispose()
+        {
+            if (_networkStream != null)
+            {
+                _networkStream.Dispose();
+                _networkStream = null;
+            }
+
+            if (_tcpClient != null)
+            {
+                _tcpClient.Close();
+                _tcpClient = null;
+            }
+        }
+    }
+}
---
+++ src/OneCog.Net.Universal/TcpClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace OneCog.Net
+{
+    public class TcpClient : ITcpClient
+    {
+        private StreamSocket _socket;
+        private DataReader _dataReader;
+        private DataWriter _dataWriter;
+
+        public async Task Connect(string host, uint port)
+        {
+            _socket = new StreamSocket();
+            await _socket.ConnectAsync(new HostName(host), port.ToString());
+            _dataReader = new DataReader(_socket.InputStream);
+            _dataWriter = new DataWriter(_socket.OutputStream);
+        }
+
+        public async Task Read(byte[] bytes)
+        {
+            if (bytes == null) throw new ArgumentNullException("bytes");
+            await _dataReader.LoadAsync((uint) bytes.Length);
+            _dataReader.ReadBytes(bytes);
+        }
+
+        public async Task Write(byte[] bytes)
+        {
+            if (bytes == null) throw new ArgumentNullException("bytes");
+            _dataWriter.WriteBytes(bytes);
+            await _dataWriter.StoreAsync();
+        }
+
+        public void Dispose()
+        {
+            if (_dataReader != null)
+            {
+                _dataReader.Dispose();
+                _dataReader = null;
+            }
+
+            if (_dataWriter != null)
+            {
+                _dataWriter.Dispose();
+                _dataWriter = null;
+            }
+
+            if (_socket != null)
+            {
+                _socket.Dispose();
+                _socket = null;
+            }
+        }
+    }
+}
