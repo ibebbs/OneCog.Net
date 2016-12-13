9951ee2fe9c33099f4c6c0c066547c41e41ea7f0
Ian Bebbington <github@bebbs.co.uk>
2016-12-13 10:18:02 +0000

    Added 'Util' class to get local ip address.

---
+++ src/OneCog.Net.Desktop/Util.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace OneCog.Net
+{
+    public static class Util
+    {
+        public static IEnumerable<string> LocalHostNames()
+        {
+            return Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).Select(ip => ip.ToString());
+        }
+    }
+}
---
+++ src/OneCog.Net.Uwp/Util.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+
+namespace OneCog.Net
+{
+    public static class Util
+    {
+        public static IEnumerable<string> LocalHostNames()
+        {
+            return NetworkInformation.GetHostNames().Where(localHostName => localHostName.IPInformation != null && localHostName.Type == HostNameType.Ipv4).Select(localHostName => localHostName.ToString());
+        }
+    }
+}
